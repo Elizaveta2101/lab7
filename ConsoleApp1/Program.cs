597689c14d0f953f488ff03cbe18b6d0d1276533
Elizaveta2101 <134704666+Elizaveta2101@users.noreply.github.com>
2023-05-28 21:09:27 +0300

    zanovo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,36 +6,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double a, b;
-            char z;
-            Console.WriteLine("Введите a ");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите действие (+, -, *, /) ");
-            z = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("Введите b ");
-            b = Convert.ToDouble(Console.ReadLine());
-            switch (z)
+            string cont;
+            do
             {
-                case '+':
-                    Console.WriteLine("{0}+{1}={2}", a, b, a + b);
-                    break;
-                case '-':
-                    Console.WriteLine("{0}-{1}={2}", a, b, a - b);
-                    break;
-                case '*':
-                    Console.WriteLine("{0}*{1}={2}", a, b, a * b);
-                    break;
-                case '/':
-                    if (b == 0)
-                        Console.WriteLine("На ноль делить нельзя");
-                    else
-                    Console.WriteLine("{0}/{1}={2}", a, b, a / b);
-                    break;
-                default:
-                    Console.WriteLine("Ошибка");
-                    break;
-            }
-            Console.ReadLine();
+                double a, b;
+                char z;
+                Console.WriteLine("Введите a ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите действие (+, -, *, /) ");
+                z = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Введите b ");
+                b = Convert.ToDouble(Console.ReadLine());
+                switch (z)
+                {
+                    case '+':
+                        Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+                        break;
+                    case '-':
+                        Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                        break;
+                    case '*':
+                        Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                        break;
+                    case '/':
+                        if (b == 0)
+                            Console.WriteLine("На ноль делить нельзя");
+                        else
+                            Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка");
+                        break;
+
+                }
+                Console.WriteLine("Запустить заново");
+                cont = Convert.ToString(Console.ReadLine());
+            } while (cont == "да");
         }
     }
 }
